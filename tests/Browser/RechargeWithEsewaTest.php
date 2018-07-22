bb26a7365a004663bccf34e5fcdd8c58ae522162
suman shrestha <summonshr@gmail.com>
2018-07-22 22:30:18 +0545

    WORKING

---
+++ tests/Browser/RechargeWithEsewaTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class RechargeWithEsewaTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testEsewaWillRechargeMyBalance()
+    {
+        $this->browse(function (Browser $browser) {
+            $username = env('ESEWA_USERNAME');
+            $password = env('ESEWA_PASSWORD');
+
+            $browser->visit('https://esewa.com.np')
+            ->type('.loginPart .form-control[name=username]',$username)
+            ->type('.loginPart .form-control[name=password]', $password)
+            ->click('.loginPart [type=submit]')
+            ->pause(1000);
+
+            $browser->visit('https://esewa.com.np/#/make_payment/NTNAM/NT%20Prepaid%20Topup')
+            ->pause(1500)
+            ->type('#mobileNo','9841145614')
+            ->select('#amount',10)
+            ->pause(2000)
+            ->click('#proceedfundTransferPayment')
+            ->pause(3000)
+            ->click('div.payment-box .ng-scope .theme_box .btn-green')
+            ->pause(3000);
+        });
+    }
+}
---
+++ tests/Browser/RechargeWithKhaltiTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class RechargeWithKhaltiTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testKhaltiWillRechargeMyBalance()
+    {
+        $this->browse(function (Browser $browser) {
+            $username = env('KHALTI_USERNAME');
+            $password = env('KHALTI_PASSWORD');
+
+            $browser->visit('https://khalti.com')
+                ->pause(1000)
+                ->click('li a button.popup')
+                ->pause(1000)
+                ->type('#login-component .field [type=text]',$username)
+                ->type('#login-component .field [type=password]', $password)
+                ->click('#login-component [type=submit]')
+                ->pause(2000);
+
+            $browser->visit('https://khalti.com/#/service/topup/')
+                ->pause(2000)
+                ->type('.sixteen.wide.mobile.eight.wide.computer.column [type=text]','9841145614')
+                ->pause(1000)
+                ->click('.sixteen.wide.mobile.eight.wide.computer.column .selection')
+                ->pause(1000)
+                ->click('.visible.menu div:nth-child(1)')
+                ->pause(1000)
+                ->click('[data-component=ServiceSubmitButton]')
+                ->pause(1000)
+                ->click('.basic.buttons button:nth-child(2)')
+                ->pause(3000);
+        });
+    }
+}
